91575eb80714540bd3ab146a821599340519df4a
ecksii <ecksi@proton.me>
2024-08-27 13:51:41 +0100

    Laptop changes
    Signed-off-by: ecksii <ecksi@proton.me>

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,11 +1,12 @@
-const toggleButtonList = document.getElementsByClassName('toggle-button')[0];
-const navigation = document.getElementsByClassName('navigation')[0];
+var toggleButtonList = document.getElementsByClassName('toggle-button')[0];
+var navigation = document.getElementsByClassName('navigation')[0];
 const navClasses = navigation.classList;
 const display = document.getElementById('navid')
 
-
 toggleButtonList.addEventListener('click', () => {
+    toggleButtonList = document.getElementsByClassName('toggle-button')[0];
 
+    console.log("main nav: " + navigation)
     const result = navClasses.toggle("hidden")
     /*
     navigation.textContent = `'display: ' ${
